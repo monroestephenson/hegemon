deea15bc6f173decb1c3774808baf4d34cdca59f
monroestephenson <140461945+monroestephenson@users.noreply.github.com>
2025-02-21 16:53:34 +0100

    Clean build and remove external directory

--- build/external/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/monroestephenson/database_backup/external/googletest/googlemock
-# Build directory: /Users/monroestephenson/database_backup/build/external/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/external/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/monroestephenson/database_backup/external/googletest/googletest
-# Build directory: /Users/monroestephenson/database_backup/build/external/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
